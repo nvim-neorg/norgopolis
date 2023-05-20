f1bd8f477fd7e92fec813c75995348696b47d118
Vhyrro <vhyrro@gmail.com>
2023-05-20 21:52:01 +0200

    broken: add basic `src/lib.rs` code

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,14 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::future::Future;
+use futures::{TryFutureExt, FutureExt};
+
+use communication::{forwarder_client::ForwarderClient, Invocation, MessagePack};
+use serde::Deserialize;
+use tonic::{transport::Channel, Request, Status, Response};
+
+mod communication {
+    tonic::include_proto!("communication");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+struct ConnectionHandle(ForwarderClient<Channel>);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ConnectionHandle {
+    pub fn invoke_raw<'a>(&'a mut self, module: String, function_name: String, args: Option<MessagePack>) -> impl Future<Output = Result<Response<MessagePack>, Status>> + 'a  {
+        self.0.forward(Request::new(Invocation {
+            module,
+            function_name,
+            args,
+        }))
     }
+
+    pub fn invoke<'a, 'b, TargetStruct>(&'a mut self, module: String, function_name: String, args: Option<MessagePack>) -> impl Future<Output = Result<TargetStruct, rmp_serde::decode::Error>> + 'a 
+        where TargetStruct: Deserialize<'a>,
+    {
+        self.invoke_raw(module, function_name, args).map(|response| {
+            let slice = response.unwrap().into_inner().data.into_boxed_slice();
+            rmp_serde::from_slice::<'b, TargetStruct>(slice.as_ref())
+        })
+    }
+}
+
+pub async fn connect(ip: String, port: String) -> anyhow::Result<ConnectionHandle> {
+    // TODO: Spin up the server if it doesn't already exist
+    // NOTE: Perhaps make server spinup a feature flag?
+    Ok(ConnectionHandle(ForwarderClient::connect(ip + ":" + &port).await?))
 }
