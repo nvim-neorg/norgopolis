a8084aaffedde701a0bf8a5f19bcffe988504797
Vhyrro <vhyrro@gmail.com>
2023-07-06 18:29:56 +0200

    feat(norgopolis-module): add `encode` function for `MessagePack`

--- norgopolis-module/src/module_communication.rs
+++ norgopolis-module/src/module_communication.rs
@@ -2,6 +2,7 @@ tonic::include_proto!("module_communication");
 
 use rmp_serde;
 use anyhow::Result;
+use serde::Serialize;
 
 impl MessagePack {
     pub fn decode<Target>(self) -> Result<Target>
@@ -9,5 +10,11 @@ impl MessagePack {
         Target: serde::de::DeserializeOwned,
     {
         Ok(rmp_serde::decode::from_slice(self.data.as_slice())?)
+    }
+
+    pub fn encode<T>(target: T) -> Result<MessagePack>
+        where T: Serialize
+    {
+        Ok(MessagePack { data: rmp_serde::encode::to_vec(&target)? })
     }
 }
